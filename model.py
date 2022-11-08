36ee11299cfcc9f12f5eb7e6a29ace8aa7299b32
sinq <zox004@gmail.com>
2022-11-08 10:14:15 +0900

    get images

---
+++ model.py
@@ -0,0 +1,20 @@
+from PIL import Image
+# %matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision import transforms
+import numpy as np
+
+trans = transforms.Compose([transforms.Resize((100,100)),
+                            transforms.ToTensor(),
+                            transforms.Normalize((0.5, 0.5, 0.5), (0.5,0.5,0.5))
+                            ])
+trainset = torchvision.datasets.ImageFolder(root = "/Users/sinq/Desktop/PythonWorkspace/AITeacher/images",
+                                            transform = trans)
+print(trainset.__getitem__(5))
+
+classes = trainset.classes
+print(classes)
